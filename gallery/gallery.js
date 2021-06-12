b8b89b73bd443cdc8ad21d2c8cb501298f039602
theEmelie <theem@live.se>
2021-06-12 21:33:05 +0100

    added and fixed position on cycle images

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -1,27 +1,28 @@
-function galleryExpandImage(image) {
-  // Get the expanded image
-  var expandImage = document.getElementById("expanded-image");
-  // Get the image text
-  var caption = document.getElementById("caption");
-  // Use the same src in the expanded image as the image being clicked on from the grid
-  expandImage.src = image.src;
-  // Use the value of the alt attribute of the clickable image as text inside the expanded image
-  caption.innerHTML = image.alt;
-  // Show the container element (hidden with CSS)
-  expandImage.parentElement.style.display = "block";
-}
-
-
-// function galleryExpandImage(imgs) {
+// function galleryExpandImage(image) {
 //   // Get the expanded image
-//   var expandedImage = $('#expanded-image')
+//   var expandImage = document.getElementById("expanded-image");
 //   // Get the image text
-//   var imgText = $('#caption');
+//   var caption = document.getElementById("caption");
 //   // Use the same src in the expanded image as the image being clicked on from the grid
-//   $('image-col').prop('src')
+//   expandImage.src = image.src;
 //   // Use the value of the alt attribute of the clickable image as text inside the expanded image
-//   imgText.html(imgs.alt);
+//   caption.innerHTML = image.alt;
 //   // Show the container element (hidden with CSS)
-//   expandedImage.css("display", "block");
-//
+//   expandImage.parentElement.style.display = "block";
 // }
+//
+
+function galleryExpandImage(imgs) {
+
+  var expandedImage = $('#expanded-image')
+  var imgText = $('#caption');
+
+  var loc = imgs.src;
+  console.log(loc);
+  expandedImage.prop("src", loc);
+  console.log(expandedImage.prop("src"));
+  imgText.html(imgs.alt);
+
+  expandedImage.css("display", "block");
+
+}
