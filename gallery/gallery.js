66596a2de983b0662d718f841b940c749d1e1fce
theEmelie <theem@live.se>
2021-06-16 01:11:52 +0100

    added comments

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -4,8 +4,11 @@ function galleryExpandImage(imgs) {
   let imgText                = $('#caption');
   let loc                    = imgs.src;
   // console.log(loc);
+  // Get the selected image url
   expandedImage.prop("src", loc);
   // console.log(expandedImage.prop("src"));
+  // Display the image alternate caption
   imgText.html(imgs.alt);
+  // Set the expanded image to display block as it is set to none in the css.
   expandedImageContainer.css("display", "block");
 }
