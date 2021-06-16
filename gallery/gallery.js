0f6f32877c9881e108d8bceee35700e27831867c
theEmelie <theem@live.se>
2021-06-16 01:07:50 +0100

    made changes to the jquery code to make it work and moved img captions around

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -1,27 +1,11 @@
-// function galleryExpandImage(image) {
-//   // Get the expanded image
-//   var expandImage = document.getElementById("expanded-image");
-//   // Get the image text
-//   var caption = document.getElementById("caption");
-//   // Use the same src in the expanded image as the image being clicked on from the grid
-//   expandImage.src = image.src;
-//   // Use the value of the alt attribute of the clickable image as text inside the expanded image
-//   caption.innerHTML = image.alt;
-//   // Show the container element (hidden with CSS)
-//   expandImage.parentElement.style.display = "block";
-// }
-
-
 function galleryExpandImage(imgs) {
-
-  let expandedImage = $('#expanded-image')
-  let imgText = $('#caption');
-
-  let loc = imgs.src;
-  console.log(loc);
+  let expandedImage          = $('#expanded-image')
+  let expandedImageContainer = $('.expanding-image-container')
+  let imgText                = $('#caption');
+  let loc                    = imgs.src;
+  // console.log(loc);
   expandedImage.prop("src", loc);
-  console.log(expandedImage.prop("src"));
+  // console.log(expandedImage.prop("src"));
   imgText.html(imgs.alt);
-
-  expandedImage.css("display", "block");
+  expandedImageContainer.css("display", "block");
 }
